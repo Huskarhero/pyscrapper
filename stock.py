7881cab175b342c7693e353016ad0162e4df48da
Huskarhero <stevenjobs207@gmail.com>
2013-10-29 01:57:02 -0700

    fixed symbol case

--- stock.py
+++ stock.py
@@ -1,21 +1,16 @@
 import urllib.request
 import re
 
-symbolfile = open("symbols1.txt")
-
-symbolslist = symbolfile.read()
-
-newsymbolslist = symbolslist.split("\n")
-
+symbolslist = ["aapl","spy","goog","nflx"]
 i = 0
-while i < len(newsymbolslist):
-	url = "http://in.finance.yahoo.com/q?s=" + newsymbolslist[i] 
+while i < len(symbolslist):
+	url = "http://in.finance.yahoo.com/q?s=" + symbolslist[i] 
 	htmlfile = urllib.request.urlopen(url)
 	htmltext = htmlfile.read().decode('utf-8')
 
-	regex = '<span id="yfs_l84_[^.]*">(.+?)</span>'
+	regex = '<span id="yfs_l84_'+ symbolslist[i] + '">(.+?)</span>'
 	pattern = re.compile(regex)
 	price = re.findall(pattern,htmltext)
 
-	print ("the price of",newsymbolslist[i],"is",price)
+	print ("the price of",symbolslist[i],"is",price)
 	i += 1
